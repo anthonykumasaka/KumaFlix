3a6a51bdcf5de6546a589c58f10e348fe3b8b7f6
anthonykumasaka <anthonykumasaka@gmail.com>
2018-12-14 13:10:31 -0800

    please work heroku

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,25 +1,29 @@
-import React from 'react'; 
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Splash = () => (
 
   <div>
-    <div className="splash-flex"> 
+    <div className="splash-flex">
       <div className="black-overlay-flex-1">
-          <div className="netflix"/>
-          <div className="login-link-2">
-          <Link className="on-link" to='/login'>Sign In</Link>
-        </div> 
+        <div className="netflix" />
+        <div className="login-link-2">
+          <div className="on-link-2">
+            <Link className="on-link" to='/login'>Sign In</Link>
+          </div>
+        </div>
       </div>
       <div className="black-overlay-flex-splash">
         <h1>Watch on any device</h1>
         <h2>Stream on your phone, tablet, laptop, and TV <br />  without paying more.</h2>
         <div className="login-link">
-          <Link className="on-link" to='/signup'>JOIN FREE FOR A MONTH</Link>
+          <div className="on-link">
+            <Link className="on-link" to='/signup'>JOIN FREE FOR A MONTH</Link>
+          </div>
         </div>
       </div>
     </div>
-  </div> 
-); 
+  </div>
+);
 
 export default Splash; 
