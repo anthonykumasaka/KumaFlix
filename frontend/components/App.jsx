645d69edf35d5f2cfb29f14dd5218d193ea7aae2
anthonykumasaka <anthonykumasaka@gmail.com>
2018-12-13 16:26:54 -0800

    add App

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -22,7 +22,6 @@ const App = ({store}) => (
         </Switch>
       </HashRouter>
     </Provider>
-
 );
 
 export default App;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import App from './components/app';
+import App from './components/App';
 import {login} from './actions/session_actions'; 
 
 
