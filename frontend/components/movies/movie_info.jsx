5fc6f1fb6ab6f807fac9dc66cc750197c140949f
anthonykumasaka <anthonykumasaka@gmail.com>
2019-03-31 18:19:19 -0800

    Refactor Kumaflix

--- frontend/components/movies/movie_info.jsx
+++ frontend/components/movies/movie_info.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const MovieInfo = props => {
-
   if (props.id){
     return (
       // <div style={{backgroundImage: `url(${props.movies[props.id].photo})`}}  className="movie-info">    
       <div className="info-title">
             <div className='title-description-text'>
               <h2>{props.movies[props.id].title}</h2>
-                <br/>
+              <br/>
               <h3>{props.movies[props.id].description}</h3>
               <br/>
               <h4>{props.movies[props.id].year}</h4> 
