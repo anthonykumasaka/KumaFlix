e566c1f6ca349c6bd0e8d6a9fe10ef7146f0d82c
anthonykumasaka <anthonykumasaka@gmail.com>
2019-04-01 18:19:19 -0800

    Refactor Kumaflix

--- frontend/components/movies/movie_info.jsx
+++ frontend/components/movies/movie_info.jsx
@@ -6,13 +6,13 @@ const MovieInfo = props => {
     return (
       // <div style={{backgroundImage: `url(${props.movies[props.id].photo})`}}  className="movie-info">    
       <div className="info-title">
-            <div className='title-description-text'>
-              <h2>{props.movies[props.id].title}</h2>
-              <br/>
-              <h3>{props.movies[props.id].description}</h3>
-              <br/>
-              <h4>{props.movies[props.id].year}</h4> 
-            </div>
+        <div className='title-description-text'>
+          <h2>{props.movies[props.id].title}</h2>
+          <br/>
+          <h3>{props.movies[props.id].description}</h3>
+          <br/>
+          <h4>{props.movies[props.id].year}</h4> 
+        </div>
             <div className="kumaflix-original-title"></div>
               <Link to={`/play/${props.movies[props.id].id}`}>
               <button>Play</button>
@@ -20,7 +20,7 @@ const MovieInfo = props => {
               <button className="btn-my-list">My List</button>
          
              <img src={props.movies[props.id].photo}/>
-            <div className="movie-gradient"></div>
+        <div className="movie-gradient"></div>
       </div> 
         
   ); 
